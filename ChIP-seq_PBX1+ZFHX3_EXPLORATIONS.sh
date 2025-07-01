59f06a3c101adf564de0972656fe4306e8858d57
veritasnondatur <117276541+veritasnondatur@users.noreply.github.com>
2025-06-30 18:16:54 -0700

    Add files via upload

--- ChIP-seq_PBX1+ZFHX3_EXPLORATIONS.sh
+++ ChIP-seq_PBX1+ZFHX3_EXPLORATIONS.sh
@@ -8,13 +8,27 @@ BED1="/Users/veralaub/Documents/postdoc/bioinformatics/data/ChIP-seq/midface/MF_
 BED2="/Users/veralaub/Documents/postdoc/bioinformatics/data/ChIP-seq/midface/E115-ZFHX3_1_peaks.bed"
 BED3="/Users/veralaub/Documents/postdoc/bioinformatics/data/ChIP-seq/midface/E115-ZFHX3_2_peaks.bed"
 BED4="/Users/veralaub/Documents/postdoc/bioinformatics/data/ChIP-seq/midface/E115-ZFHX3_3_peaks.bed"
+BED5="/Users/veralaub/Documents/postdoc/bioinformatics/data/ChIP-seq/midface/E115Epi-PBX1_1_peaks.bed"
+BED6="/Users/veralaub/Documents/postdoc/bioinformatics/data/ChIP-seq/midface/E115Epi-PBX1_2_peaks.bed"
 
-# Output file
-OUTPUT="/Users/veralaub/Documents/postdoc/bioinformatics/results/ChIP-seq_PBX1+ZFHX3_MF_E11.5/ChIP-seq_PBX1+ZFHX3_E11.5MF_multiinter_output.bed"
+# Output files
+OUTPUT1="/Users/veralaub/Documents/postdoc/bioinformatics/results/ChIP-seq_PBX1+ZFHX3_MF_E11.5/ChIP-seq_PBX1+ZFHX3_bothE11.5MF/ChIP-seq_PBX1+ZFHX3_E11.5MF_multiinter_output.bed"
+OUTPUT2="/Users/veralaub/Documents/postdoc/bioinformatics/results/ChIP-seq_PBX1+ZFHX3_MF_E11.5/ChIP-seq_PBX1_E11.5Epi+ZFHX3_E11.5MF/ChIP-seq_PBX1_E11.5Epi+ZFHX3_E11.5MF_multiinter_output.bed"
 
-# Run bedtools multiinter
+# Run bedtools multiinter for whole MF datasets
 echo "Running bedtools multiinter..."
-bedtools multiinter -i "$BED1" "$BED2" "$BED3" "$BED4" > "$OUTPUT"
+bedtools multiinter -i "$BED1" "$BED2" "$BED3" "$BED4" > "$OUTPUT1"
 
-echo "Intersection completed."
-echo "Results saved to: $OUTPUT"
+echo "Intersection of whole MF datasets completed."
+echo "Results saved to: $OUTPUT1"
+
+
+# Run bedtools multiinter for E11.5 PBX1 Epi and ZFHX3 whole MF datasets
+echo "Running bedtools multiinter..."
+bedtools multiinter -i "$BED5" "$BED6" "$BED2" "$BED3" "$BED4" > "$OUTPUT2"
+
+echo "Intersection of E11.5 PBX1 Epi and ZFHX3 whole MF datasets completed."
+echo "Results saved to: $OUTPUT2"
+
+
+
